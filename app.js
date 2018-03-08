ca955e55a01e2a8444c11dc7b4ff9ef15b9033fb
Amber Burroughs <amberlburroughs@gmail.com>
2018-03-08 13:56:19 -0800

    update

--- app.js
+++ app.js
@@ -28,28 +28,10 @@ function doWhatUserWants(){
       `)
      $("#submit").text("loading....")
       switch(selectedCat){
-          case "youtube":
-            var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z"
-           }); 
-           // execute the request
-            request.execute(function(response) {
-              $("#search").val("");
-              //console.log(response);
-              ytResults = response.items;
-              console.log(results)
-              var current = [ytResults[0]]
-              var index = 0;
-              displayVideo(current, index)
 
-              resetVideoHeight();
-              $(window).on("resize", resetVideoHeight);
-           });
+          case "youtube":
+          console.log(selectedCat);
+          
           break;
       }
   });
