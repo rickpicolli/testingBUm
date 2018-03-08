8b417a5baad7162c47d9f4eb950e73e6c8aa66e9
Amber Burroughs <amberlburroughs@gmail.com>
2018-03-08 14:21:25 -0800

    update

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ $(document).on("click", ".catBtn", function(){
 })
 
 
-function tplawesome(e,t){
-  res=e;for(var n=0;n<t.length;n++){
-    res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){
-      return t[n][r]
-    })
-  }return res
-}
+
 
 $("form").on("submit", function(e) {
    e.preventDefault();
@@ -31,32 +25,36 @@ $("form").on("submit", function(e) {
 
         case "youtube":
         console.log(selectedCat);
-          var request = gapi.client.youtube.search.list({
-          part: "snippet",
-          type: "video",
-          q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-          maxResults: 3,
-          order: "viewCount",
-          publishedAfter: "2015-01-01T00:00:00Z"
-         }); 
-         // execute the request
-          request.execute(function(response) {
-            $("#search").val("");
-            //console.log(response);
-            ytResults = response.items;
-            console.log(results)
-            var current = [ytResults[0]]
-            var index = 0;
-            displayVideo(current, index)
-
-            resetVideoHeight();
-            $(window).on("resize", resetVideoHeight);
-         });
+         getVideo();
         break;
     }
 });
 
+/*////////////////////// YOUTUBE ///////////////////////////////*/
 
+function getVideo(){
+   var request = gapi.client.youtube.search.list({
+    part: "snippet",
+    type: "video",
+    q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+    maxResults: 3,
+    order: "viewCount",
+    publishedAfter: "2015-01-01T00:00:00Z"
+   }); 
+   // execute the request
+  request.execute(function(response) {
+    $("#search").val("");
+    //console.log(response);
+    ytResults = response.items;
+    console.log(results)
+    var current = [ytResults[0]]
+    var index = 0;
+    displayVideo(current, index)
+
+    resetVideoHeight();
+    $(window).on("resize", resetVideoHeight);
+  });
+}
 
 function displayVideo(current, i){
   $("#submit").val("search")
@@ -79,37 +77,39 @@ function displayVideo(current, i){
   });
 }
 
-// create buttons for results and add them to a wrapper
-function createButtons(i){
-  var $btnWrap = $("<div>").addClass("resBtnWrap");
-
-  var $next = $("<button>")
-  $next.text("next").addClass("resultBtn nextBtn")
-  if(i === 3){
-    $next.addClass("hide");
-  }else{
-    $next.removeClass("hide");
-  }
-  
-  var $clear = $("<button>")
-  $clear.text("clear").addClass("resultBtn clearBtn")
-  
-  var $save = $("<button>")
-  $save.text("save").addClass("resultBtn saveBtn")
-
-  return $($btnWrap).append($next, $clear, $save)
-}
-
 // for youtube
 function resetVideoHeight() {
     $(".video").css("height", $("#results").width() * 9/16);
 }
+
+function nextYoutube(){
+  // youtube 
+      var current = $(".currentVideo").data("video");
+      var next = [ytResults[current+1]]
+      var i = current+1
+      if(current < 3){
+        $(".nextBtn").removeClass("hide");
+        displayVideo(next, i);
+      }
+}
+
+function tplawesome(e,t){
+  res=e;for(var n=0;n<t.length;n++){
+    res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){
+      return t[n][r]
+    })
+  }return res
+}
+
 function init(){
     gapi.client.setApiKey("AIzaSyD0-_9xMJHK9CcHCP7JET7Pv3rSuYu3KUY");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
 }
+
+/*//////////////////////END YOUTUBE ///////////////////////////////*/
+
 
 // click for any of the result buttons (next, clear, save)
 $(document).on("click", ".resultBtn", function(){
@@ -132,19 +132,28 @@ $(document).on("click", ".resultBtn", function(){
 
   }
 })
+// create buttons for results and add them to a wrapper
+function createButtons(i){
+  var $btnWrap = $("<div>").addClass("resBtnWrap");
 
+  var $next = $("<button>")
+  $next.text("next").addClass("resultBtn nextBtn")
+  if(i === 3){
+    $next.addClass("hide");
+  }else{
+    $next.removeClass("hide");
+  }
+  
+  var $clear = $("<button>")
+  $clear.text("clear").addClass("resultBtn clearBtn")
+  
+  var $save = $("<button>")
+  $save.text("save").addClass("resultBtn saveBtn")
 
-function nextYoutube(){
-  // youtube 
-      var current = $(".currentVideo").data("video");
-      var next = [ytResults[current+1]]
-      var i = current+1
-      if(current < 3){
-        $(".nextBtn").removeClass("hide");
-        displayVideo(next, i);
-      }
+  return $($btnWrap).append($next, $clear, $save)
 }
 
+// reset 
 function resetResults(){
   ytResults = null;
   selectedCat = null
