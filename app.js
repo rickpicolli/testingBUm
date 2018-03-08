f911d4de58e76b6d1cc050459d9ffe78ee45f21a
Amber Burroughs <amberlburroughs@gmail.com>
2018-03-08 11:37:14 -0800

    update

--- app.js
+++ app.js
@@ -2,52 +2,61 @@
 // require("dotenv").config();
 // var keys = require("./keys.js");
 var ytResults;
-  function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
-  $(function() {
-    $("form").on("submit", function(e) {
-       e.preventDefault();
-       // prepare the request
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z"
-       }); 
-       // execute the request
-        request.execute(function(response) {
+function tplawesome(e,t){
+  res=e;for(var n=0;n<t.length;n++){
+    res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){
+      return t[n][r]
+    })
+  }return res
+}
 
-          //console.log(response);
-          ytResults = response.items;
-          console.log(results)
-          var current = [ytResults[0]]
-          var index = 0;
-          displayVideo(current, index)
+// on submit for youtube
+$(function() {
+  $("form").on("submit", function(e) {
+     e.preventDefault();
+     // prepare the request
+     var request = gapi.client.youtube.search.list({
+          part: "snippet",
+          type: "video",
+          q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+          maxResults: 3,
+          order: "viewCount",
+          publishedAfter: "2015-01-01T00:00:00Z"
+     }); 
+     // execute the request
+      request.execute(function(response) {
 
-          resetVideoHeight();
-       });
-    });
-    
-    $(window).on("resize", resetVideoHeight);
+        //console.log(response);
+        ytResults = response.items;
+        console.log(results)
+        var current = [ytResults[0]]
+        var index = 0;
+        displayVideo(current, index)
+
+        resetVideoHeight();
+     });
   });
+  
+  $(window).on("resize", resetVideoHeight);
+});
 
 function displayVideo(current, i){
   $("#results").html("");
-    var ytWrap = $("<div>")
-    ytWrap.attr("data-video", i)
+    var $ytWrap = $("<div>")
+    $ytWrap.addClass("currentVideo")
+    $ytWrap.attr("data-video", i)
     console.log(current)
 
   $.each(current, function(index, item) {
     console.log(index, item);
     $.get("item.html", function(data) {
-        $(ytWrap).append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+        $($ytWrap).append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
     });
     // save wrapper with result buttons to a variable
     var btns = createButtons()
     // ad video then wrap with buttons to page
-    $("#results").append(ytWrap, btns);
+    $("#results").append($ytWrap, btns);
 
   });
 }
@@ -68,13 +77,34 @@ function createButtons(){
   return $($btnWrap).append($next, $clear, $save)
 }
 
+// for youtube
 function resetVideoHeight() {
     $(".video").css("height", $("#results").width() * 9/16);
 }
-
 function init(){
     gapi.client.setApiKey("AIzaSyD0-_9xMJHK9CcHCP7JET7Pv3rSuYu3KUY");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
 }
+
+
+$(document).on("click", ".resultBtn", function(){
+  var selectedBtn = $(this).text()
+  var current = $(".currentVideo").data("video");
+  var next = [ytResults[current+1]]
+  var i = current+1
+  switch(selectedBtn){
+    case "next":
+      if (current < 3){
+        displayVideo(next, i);
+      }
+      // stop 
+    break;
+    case "clear":
+    break
+    case "save":
+    break
+
+  }
+})
